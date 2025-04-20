4383cfb49eeeaa0790d11129a4d65a4cabf8ea9a
Jeffreasy <158185183+Jeffreasy@users.noreply.github.com>
2025-04-20 23:46:46 +0200

    debug: Use any for BlogDetailPage props to test build

--- src/app/(pages)/blog/[slug]/page.tsx
+++ src/app/(pages)/blog/[slug]/page.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 
-// Revert to Props type similar to ProjectDetailPage
-type Props = {
-  params: { slug: string };
-  // searchParams removed for now to match ProjectDetailPage structure
-};
-
-export default function BlogDetailPage({ params }: Props) {
-  return <div>Blog Post Detail Page for: {params.slug}</div>;
+// Tijdelijk 'any' gebruiken voor debuggen van de build error
+export default function BlogDetailPage({ params }: any) {
+  return <div>Blog Post Detail Page for: {params?.slug || 'unknown slug'}</div>;
 } 
