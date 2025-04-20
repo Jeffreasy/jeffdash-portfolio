82c73a9bd1a23789bd7852252aba1ada41d87603
Jeffreasy <158185183+Jeffreasy@users.noreply.github.com>
2025-04-20 18:19:04 +0200

    Fix: Add searchParams to Props type in blog detail page

--- src/app/(pages)/blog/[slug]/page.tsx
+++ src/app/(pages)/blog/[slug]/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 // Params type to access the slug
 type Props = {
   params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
 
 export default function BlogDetailPage({ params }: Props) {
